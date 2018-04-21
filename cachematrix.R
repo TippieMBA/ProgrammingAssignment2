ca3e373862921e72bfdf669ccd2e5b1f6723bb3a
Rajeev Kumar <rajeev.kumar2@cvshealth.com>
2018-04-21 01:42:39 -0500

    Add exisiting file

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,48 @@
 makeCacheMatrix <- function(i = matrix()) {
-  inva = NULL
-  setvalue = function(j) {
+  inva <- NULL
+  #setting up the matrix
+  setvalue <- function(j) {
       i <<- j
     inva <<- NULL
   }
-  getvalue = function() i
-  setinva = function(inverse) inva <<- inverse 
-  getinva = function() inva
+  #retrieving the matrix
+  getvalue <- function() i
+  
+  #setting up the inverse of the marix
+  setinva <- function(inverse) inva <<- inverse 
+  
+  #getting the inverse of the matrix
+  getinva <- function() inva
+  
+  #returning the list of the methods
   list(setvalue=setvalue, getvalue=getvalue, setinva=setinva, getinva=getinva)
 }
 
+## Computing the inverse of the  matrix returned by "makeCacheMatrix"
+## If the inverse has already been calculated,then the "cachesolve" 
+## should retrieve the inverse from the cache
+
 cacheSolve <- function(i, ...) {
   
-  inva = i$getinva()
-  #retrieving cache value of the inverse of matrix
+  ## Returning  a matrix that is the inverse of 'i'
+  inva <- i$getinva()
+  #retrieving the inverse of the matrix if it is already created and set
   if (!is.null(inva)){
       message("getting cached data")
     return(inva)
   }
   
-   indata = i$getvalue()
+  #get the matrix from the object.
+   indata <- i$getvalue()
+   
   ##calculating inverse of the matrix if it is not already in cache
-  inva = solve(indata)
+  inva <- solve(indata)
   i$setinva(inva)
    return(inva)
 }
 
 m <- matrix(rnorm(9),3,3)
 m1 <- makeCacheMatrix(m)
+
+#return the matrix
 cacheSolve(m1)
